c92c2a0b86bdaf904e9d60d595dd120e716d92db
Farhan Ahamed <farhanahamed717@gmail.com>
2026-03-28 19:27:21 +0000

    add user system

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+users = []
+
 @app.get("/")
 def home():
     return {"message": "API working"}
-    
+
+@app.post("/register")
+def register(user: dict):
+    users.append(user)
+    return {"message": "User added"}
+
+@app.get("/user/{phone}")
+def get_user(phone: str):
+    for u in users:
+        if u["phone"] == phone:
+            return u
+    return {"detail": "Not Found"}
